299a414a51002f402e329d930ef7a375c495e387
Vitor Ripper <88108629+vitorripper@users.noreply.github.com>
2022-09-29 15:46:56 -0300

    .

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,8 +10,17 @@ function Home(){
         <div className="homep">
             
         <img className="imagemfundo" src= {cartao}/>
+        
         <div className="texto">
-        <h1 id="titulo">Home</h1>
+        <h1 id="txt2">Home</h1>
+        <br></br>
+        <span>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </span>
+        
         </div> 
                 
     
