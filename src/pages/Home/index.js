b11845db0db01ccc4701a2babf3086b76492251a
Vitor Ripper <88108629+vitorripper@users.noreply.github.com>
2022-09-29 22:36:12 -0300

    2/3

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import './banco.css';
-import cartao from'./banco.jpg';
+import cartao from'./cartaodecredito.jpg';
 
 function Home(){
 
@@ -8,20 +8,19 @@ function Home(){
 
     return(
         <div className="homep">
-        
+            
         <img className="imagemfundo" src= {cartao}/>
         
         <div className="texto">
-        <h1 id="txt2">Home</h1>
-        <br></br>
-        <span>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-        </span>
-        
-        
+            <h1 id="txt2">Home</h1>
+            <br></br>
+            <span id="textinho">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            </span>
+            <br/>
+            <input type="button" id="botao" value="Abra sua conta"/>
         </div> 
                 
     
--- src/pages/SobreNos/index.js
+++ src/pages/SobreNos/index.js
@@ -8,15 +8,18 @@ function SobreNos(){
 
     return(
         <div id="todo">
+            
             <div id="sobrenos">
-                <h1 id="titulo">QJSAHDASD</h1>
+                <h1 id="titulo">Sobre Nós</h1>
                 <p id="lorem1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum sit amet magna vel egestas. Sed ornare sem vel ipsum pretium eleifend. Integer orci massa, tincidunt id hendrerit id, vulputate vel velit. </p>
                 <p id="lorem2">Ut tempor aliquet sapien, et suscipit odio molestie sit amet. Integer sed luctus orci. Nulla nisl elit, finibus ac ante et, auctor dapibus orci. Suspendisse at arcu lorem. Integer ornare, eros eget mollis dapibus, sapien est dapibus mi, auctor semper lacus urna pellentesque eros. Suspendisse condimentum eros sit amet dolor malesuada faucibus. Duis vehicula finibus urna, vitae blandit tellus. Nunc aliquet justo in aliquam rhoncus. Pellentesque hendrerit nunc purus, blandit tincidunt lacus semper eu. Phasellus vitae nunc porta, pretium nulla in, placerat diam.</p>
             </div>
+        
 
             <div>
                 <img id="imagem" src={imagem} alt="foto-sobrenos"/>
             </div>
+            
         </div>
     )
 }
